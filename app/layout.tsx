345340f615961cae9751dea1f2328238da3f334c
0xJeu <stephane.jeudy@icloud.com>
2024-08-04 15:52:39 -0500

    edited layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import RemoveGrammarly from "@/components/RemoveGrammarly";
+import RemoveGrammarly from "@/Components/RemoveGrammarly";
 
 const inter = Inter({ subsets: ["latin"] });
 
